247f2524312dd46fc20d6028b56948cde9c30487
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-05-19 13:01:07 +0100

    Home page

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import './Button.css'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Buttons extends Component {
-    render() {
-        return (
-            <div className="row font-weight-bolder" id="buttons">
-                <div className="col-md-4 mt-3">
-                    <Link to = '/work' className="btn btn-outline-secondary">
-                        WORK
-                    </Link>
-                </div>
-                <div className="col-md-4 mt-3
-                ">
-                <Link to ='/portfolio' className="btn btn-outline-secondary">
-                        ABOUT
-                    </Link>
-                </div>
-                <div className="col-md-4 mt-3">
-                <Link to= '/Contact' className="btn btn-outline-secondary">
-                        CONTACT
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='row font-weight-bolder buttons'>
+        <div className='col-md-4 mt-4'>
+          <Link to='/work' className='btn btn-outline-secondary'>
+            WORK
+          </Link>
+        </div>
+        <div
+          className='col-md-4 mt-4
+                '
+        >
+          <Link to='/portfolio' className='btn btn-outline-secondary'>
+            ABOUT
+          </Link>
+        </div>
+        <div className='col-md-4 mt-4'>
+          <Link to='/Contact' className='btn btn-outline-secondary'>
+            CONTACT
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Buttons
+export default Buttons;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Buttons from './Buttons';
-import './Home.css'
+import './Home.css';
 
 export class Home extends Component {
-    render() {
-        return (
-            <div className="background">
-            <div className="overlay">
-            <div className="container">
-              <div className="jumbotron text-center text-white">
-                <div className="piper">
-                    <h1>Oluwapelumi Gold </h1> 
-                    <hr/>
-                    <Buttons/>
-                </div>
-              </div>
+  render() {
+    return (
+      <div className='homePage'>
+        <div className='icons'>
+          {/* <h1>Oluwapelumi Gold </h1>
+          <hr />
+          <Buttons /> */}
+
+          <div className='row'>
+            <div className='col-md-6'>
+              <img src='../images/IMG-20190204-WA0003.jpg' alt='' />
             </div>
+            <div className='col-md-6'></div>
           </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
