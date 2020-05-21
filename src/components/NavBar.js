8fcbf9b50e8237b3784838aebab413c8746e4dd5
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-05-21 20:27:09 +0100

    feature/failedresume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,48 +4,48 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-      <nav class='navbar navbar-expand-md navbar-dark bg-dark px-0'>
-        <Link to='/' class='navbar-brand mx-3 font-weight-bold'>
+      <nav className='navbar navbar-expand-md navbar-dark bg-dark px-0'>
+        <Link to='/' className='navbar-brand mx-3 font-weight-bold'>
           Oluwapelumi Gold
         </Link>
 
         <button
-          class='navbar-toggler navbar-toggler-white'
+          className='navbar-toggler navbar-toggler-white'
           data-toggle='collapse'
           data-target='#myNavbar'
         >
-          <span class='navbar-toggler-icon'></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
 
-        <div class='collapse navbar-collapse  px-4 py-3' id='myNavbar'>
-          <ul class='navbar-nav ml-auto'>
-            <li class='nav-item active'>
-              <a href='#about' class='nav-link text-white'>
+        <div className='collapse navbar-collapse  px-4 py-3' id='myNavbar'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item active'>
+              <a href='#about' className='nav-link text-white'>
                 ABOUT
               </a>
             </li>
-            <li class='nav-item'>
-              <a href='#services' class='nav-link text-white'>
+            <li className='nav-item'>
+              <a href='#services' className='nav-link text-white'>
                 SERVICES
               </a>
             </li>
-            <li class='nav-item'>
-              <a href='#resume' class='nav-link text-white'>
+            <li className='nav-item'>
+              <a href='#resume' className='nav-link text-white'>
                 RESUME
               </a>
             </li>
-            <li class='nav-item'>
-              <a href='#portfolio' class='nav-link text-white'>
+            <li className='nav-item'>
+              <a href='#portfolio' className='nav-link text-white'>
                 PORTFOLIO
               </a>
             </li>
-            <li class='nav-item'>
-              <a href='#techstack' class='nav-link text-white'>
+            <li className='nav-item'>
+              <a href='#techstack' className='nav-link text-white'>
                 TECH STACK
               </a>
             </li>
-            <li class='nav-item'>
-              <a href='#contact' class='nav-link text-white'>
+            <li className='nav-item'>
+              <a href='#contact' className='nav-link text-white'>
                 CONTACT
               </a>
             </li>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,34 @@ import React from 'react';
 export default function Resume() {
   return (
     <>
-      <section id='resume'></section>
+      <section id='resume' className='resume'>
+        <h3>Work Experience </h3>
+        <div id='card-container' className='cards'>
+          <div className='carder'>
+            <div className='inner-card'>hellow</div>
+          </div>
+          <div className='carder'>
+            <div className='inner-card'>how are you</div>
+          </div>
+          <div className='carder'>
+            <div className='inner-card'>fine</div>
+          </div>
+
+          <div className='carder'>
+            <div className='inner-card'>its okay</div>
+          </div>
+        </div>
+
+        <div className='navigation'>
+          <button id='prev' className='nav-button'>
+            Prev
+          </button>
+          <div id='current'></div>
+          <button id='next' className='nav-button'>
+            Next
+          </button>
+        </div>
+      </section>
     </>
   );
 }
